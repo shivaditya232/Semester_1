4072faf84ec0488b7c4d4790f071c57f95a329a4
Shivaditya S S <shivaditya2503@gmail.com>
2025-12-08 23:50:37 +0530

    Add files via upload

---
+++ example[1].py
@@ -0,0 +1,4 @@
+limit=int(input("enter a limit"))
+for i in range(limit):
+    for j in range(10):
+        print(i+1,"X",j+1,"=",(i+1)*(j+1))
---
+++ printing_characters_in_a_string[1].py
@@ -0,0 +1,18 @@
+for x in "banana":
+    print(x)
+
+a="      hello, WorldIamshivaditya!"
+print(a.strip())
+print(a.replace("Iamshivaditya","hi"))
+if "Iamshivaditya" in a:
+    print("hello")
+print(len(a))
+print(a[::-1])
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.split())
+y="i am the danger"
+print(y.capitalize())
+
+
