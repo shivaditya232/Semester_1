4295b0b4e16b14eedb68866d5832ef1b789cbe46
Shivaditya S S <shivaditya2503@gmail.com>
2025-12-20 18:09:19 +0530

    Add files via upload

---
+++ products_descending_ordered_by_price.py
@@ -0,0 +1,6 @@
+products=[{"id":101,"name":"laptop","price":900,"stock":12},{"id":205,"name":"keyboard","price":25,"stock":85},{"id":150,"name":"monitor","price":180,"stock":30}]
+for i in range(len(products)):
+    for j in range(0,len(products)-i-1):
+        if products[j]["price"]<products[j+1]["price"]:
+            products[j],products[j+1]=products[j+1],products[j]
+print(products)
