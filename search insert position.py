8cbb03d5a2d4bfb582785b0f26dd92c61f9f7603
Shivaditya S S <shivaditya2503@gmail.com>
2025-12-25 09:34:08 +0530

    Add files via upload

---
+++ search insert position.py
@@ -0,0 +1,9 @@
+nums=[1,3,5,6]
+target=5
+for i in nums:
+    if i==target:
+        print(nums.index(i))
+    elif:
+        nums.append(target)
+        s=sorted(nums)
+        print(s.index(target))
