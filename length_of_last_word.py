b879b1b2da6e0a7468842a2688cc5e9f3eff9497
Shivaditya S S <shivaditya2503@gmail.com>
2025-12-25 09:34:42 +0530

    Add files via upload

---
+++ length_of_last_word.py
@@ -0,0 +1,8 @@
+nums=input("enter a list")
+target=(input("enter a target"))
+length = len(nums)
+for i in range(length):
+    if nums[i] >= target:
+        print(i)
+            
+    print("length")
