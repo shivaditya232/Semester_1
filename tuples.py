fbfabf1dbd70b4744a6b55d34cc5bdc002f50264
Shivaditya S S <shivaditya2503@gmail.com>
2025-12-30 14:34:13 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,17 @@
+"""fruits=("apple","banana","cherry")
+(green,yellow,red)=fruits
+print(green)
+print(yellow)
+print(red)
+thisset={"apple","banana","cherry"}"""
+""""set1={"a","b","c"}
+set2={1,2,3}
+set3=set1.union(set2)
+print(set3)
+set1.update(set2)
+print(set1)
+set1.add("1")
+print(set1)"""""
+thisdict={"brand":"Ford", "Model":"Mustand", "year":1964}
+thisdict['colour']="red"
+print(thisdict.keys())
