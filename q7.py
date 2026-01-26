16978312798f6b850f1c97c5309050947b402adf
Shivaditya S S <shivaditya2503@gmail.com>
2026-01-26 19:31:10 +0530

    Add files via upload

--- q7.py
+++ q7.py
@@ -1,8 +1,13 @@
-def fun(x):
-    if x>0:
-        x-=1
-        fun(x)
-        print(x,end=" ")
-        x-=1
-        fun(x)
-fun(4)
+fh=open("sample.txt")
+y=fh.read()
+a="24680"
+b="13579"
+for i in y:
+    if i in a:
+        s=open("even.txt","a")
+        s.write(i)
+    if i in b:
+        g=open("odd.txt","a")
+        g.write(i)
+
+        
